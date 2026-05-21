ad1926422a5b12eafba6663e50d6ed8d08b10533
Arthur Albefaro <arthuralbefaroec@gmail.com>
2026-05-21 07:46:45 -0300

    deploy para o github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/projeto-arca-gp8",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
