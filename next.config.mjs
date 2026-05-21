ac93a46bba42079f8b0e7cc10d3ac8f96e752c98
Arthur Albefaro <arthuralbefaroec@gmail.com>
2026-05-21 08:11:49 -0300

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   output: 'export',
+  basePath: '/projeto-arca-gp8',
+  assetPrefix: '/projeto-arca-gp8/',
   trailingSlash: true,
-  basePath: isProd ? '/projeto-arca-gp8' : '',
-  assetPrefix: isProd ? '/projeto-arca-gp8/' : '',
   images: {
     unoptimized: true,
   },
